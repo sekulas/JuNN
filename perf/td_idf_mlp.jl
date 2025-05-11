3e5763c738966bde27a34d300fc015804b28e431
sekulas <sekula.sebastian22@gmail.com>
2025-05-11 18:40:02 +0200

    feat: time and acc counting

--- perf/td_idf_mlp.jl
+++ perf/td_idf_mlp.jl
@@ -12,19 +12,19 @@ include("../src/nn_model.jl")
 using JLD2
 
 ###
-X_train = load("./data/imdb_dataset_prepared.jld2", "X_train")
-y_train = load("./data/imdb_dataset_prepared.jld2", "y_train")
-X_test = load("./data/imdb_dataset_prepared.jld2", "X_test")
-y_test = load("./data/imdb_dataset_prepared.jld2", "y_test")
+# X_train = load("./data/imdb_dataset_prepared.jld2", "X_train")
+# y_train = load("./data/imdb_dataset_prepared.jld2", "y_train")
+# X_test = load("./data/imdb_dataset_prepared.jld2", "X_test")
+# y_test = load("./data/imdb_dataset_prepared.jld2", "y_test")
 ###
 
 # ####
-# using Random: seed!
-# seed!(42)
-# X_train = rand(Float32, 100, 80)  # (100, 10)
-# y_train = rand([0, 1], 1, 80)       # (100,)
-# X_test = rand(Float32, 100, 20)    # (10, 10)
-# y_test = rand([0, 1], 1, 20)         # (10,)
+using Random: seed!
+seed!(42)
+X_train = rand(Float32, 100, 80)  # (100, 10)
+y_train = rand([0, 1], 1, 80)       # (100,)
+X_test = rand(Float32, 100, 20)    # (10, 10)
+y_test = rand([0, 1], 1, 20)         # (10,)
 # ####
 
 println("Data loaded successfully.")
@@ -47,7 +47,7 @@ function test(batch, sorted_graph)
 end
 
 
-function train!(batch, model, graph, lr=0.01f0)
+function train!(batch, model, graph, lr=0.001f0)
     x_batched, y_batched = batch
     x.output .= x_batched
     y.output .= y_batched
@@ -56,22 +56,86 @@ function train!(batch, model, graph, lr=0.01f0)
 
     grads = gradient(model)
 
-    for j in Iterators.drop(batch, 2) #TODO!!!: CHECK AS BATCHED 2 ELEMS
-        x.output .= inputs[j,:]
-        y.output .= targets[j,:]
+    # for j in Iterators.drop(batch, 2) #TODO!!!: CHECK AS BATCHED 2 ELEMS
+    #     x.output .= inputs[j,:]
+    #     y.output .= targets[j,:]
 
-        forward!(graph)
-        backward!(graph)
+    #     forward!(graph)
+    #     backward!(graph)
         
-        accumulate_gradients!(grads, gradient(model))
+    #     accumulate_gradients!(grads, gradient(model))
+    # end
+
+
+    # batch_len::Integer = length(batch) / 2 # TODO!!!: ZMIEN TO
+
+    update_params!(model, lr; grads, batch_len=1)
+    # forward!(graph)
+    return nothing
+end
+
+function accuracy_batch(batch, sorted_graph)
+    x_batched, y_batched = batch
+
+    x.output .= x_batched
+    forward!(sorted_graph)
+
+    y_pred = y.output
+
+    return mean((y_pred .> 0.5) .== (y_batched .> 0.5))
+end
+
+
+
+function learn!(dataset, model, sorted_graph;
+                lr, epochs::Int,
+                print_after::Int = 1000)
+
+    losses = Float32[]
+    accs   = Float32[]
+    current_count = 0
+    t_start = time()
+
+    println("Training started...")
+    for epoch in 1:epochs
+        t_epoch_start = time()
+
+        # optionally decay lr
+        if epoch % 4 == 0
+            lr /= 10
+            println("  → Epoch $epoch: decayed lr to $lr")
+        end
+
+        for batch in dataset
+            # 1) do one gradient step
+            train!(batch, model, sorted_graph, lr)
+
+            # 2) measure loss & accuracy on that batch
+            loss_val = test(batch, sorted_graph)
+            acc_val  = accuracy_batch(batch, sorted_graph)
+
+            push!(losses, loss_val)
+            push!(accs,    acc_val)
+            current_count += 1
+
+            # 3) periodic print
+            if current_count % print_after == 0
+                println("  iter $current_count → loss = $(mean(losses)) | acc = $(mean(accs))")
+                empty!(losses)
+                empty!(accs)
+            end
+        end
+
+        # epoch summary
+        epoch_time = time() - t_epoch_start
+        println("Epoch $epoch finished in $(round(epoch_time, digits=2))s; ")
+        empty!(losses)
+        empty!(accs)
+        current_count = 0
     end
 
-
-    batch_len::Integer = length(batch) / 2 # TODO!!!: ZMIEN TO
-
-    update_params!(model, lr; grads, batch_len)
-    forward!(graph)
-    return nothing
+    total_time = time() - t_start
+    println("All epochs done in $(round(total_time, digits=2)) seconds.")
 end
 
 epochs = 5
@@ -88,13 +152,6 @@ y = Variable(zeros(Float32, 1, 1), name="y")
 graph = loss(model, x, y)
 sorted_graph = topological_sort(graph)
 
-losses = []
-for i=1:epochs
-    for batch in dataset
-        train!(batch, model, sorted_graph, η)
-        loss_val = test(batch, sorted_graph)
-        println("Epoch: $i, Loss: $loss_val")
-        push!(losses, loss_val)
-    end
-end
 
+
+learn!(dataset, model, sorted_graph; lr=η, epochs, print_after=1000)
