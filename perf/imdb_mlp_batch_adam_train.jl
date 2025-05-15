0ffa4e7e2e235c4fcea6aeb6c46f7c813e3c3c98
sekulas <sekula.sebastian22@gmail.com>
2025-05-15 20:28:31 +0200

    feat: imdb_mlp_batch_adam_train

---
+++ perf/imdb_mlp_batch_adam_train.jl
@@ -0,0 +1,65 @@
+include("../src/structs.jl")
+include("../src/scalar_operators.jl")
+include("../src/broadcast_operators.jl")
+include("../src/graph.jl")
+include("../src/forward.jl")
+include("../src/backward.jl")
+include("../src/data_loader.jl")
+include("../src/nn_model.jl")
+include("../src/optimisers.jl")
+include("../src/losses.jl")
+include("../src/layers.jl")
+
+using Printf, Random
+Random.seed!(0)
+
+using JLD2
+X_train = load("./data/imdb_dataset_prepared.jld2", "X_train")
+y_train = load("./data/imdb_dataset_prepared.jld2", "y_train")
+X_test = load("./data/imdb_dataset_prepared.jld2", "X_test")
+y_test = load("./data/imdb_dataset_prepared.jld2", "y_test")
+println("X_train: ", size(X_train))
+println("y_train: ", size(y_train))
+println("X_test: ", size(X_test))
+println("y_test: ", size(y_test))
+
+X_train = Float32.(X_train)
+y_train = Float32.(y_train)
+X_test = Float32.(X_test)
+y_test = Float32.(y_test)
+
+
+batch_size = 16
+
+dataset = DataLoader((X_train, y_train), batchsize=batch_size, shuffle=true)
+testset = DataLoader((X_test, y_test), batchsize=batch_size, shuffle=false)
+
+input_size = size(X_train, 1)
+
+model = Chain(
+    Dense((input_size => 32), ReLU, name="hidden_layer"),
+    Dense((32 => 1), σ, name="output_layer")
+)
+
+function compute_accuracy(pred, actual)
+    if length(pred) > 1
+        return mean((pred .> 0.5f0) .== (actual .> 0.5f0))
+    else
+        return (pred[1] > 0.5f0) == (actual[1] > 0.5f0) ? 1.0f0 : 0.0f0
+    end
+end
+
+net = NeuralNetwork(model, Adam(), binary_cross_entropy, compute_accuracy)
+
+epochs = 1000
+for epoch in 1:epochs
+    t = @elapsed begin
+        train_loss, train_acc = train!(net, dataset)
+    end
+    
+    test_loss, test_acc = evaluate(net, testset)
+    @printf("Epoch %d/%d: Train Loss: %.4f, Train Acc: %.4f, Test Loss: %.4f, Test Acc: %.4f, Time: %.2fs\n",
+            epoch, epochs, train_loss, train_acc, test_loss, test_acc, t)
+end
+
+
